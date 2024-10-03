2b91dfad86b6146f56760678258be1fd3bbf6150
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-03 17:32:24 +0200

    created springboot project

---
+++ src/main/java/com/example/websocket/HelloMessage/HelloMessage.java
@@ -0,0 +1,14 @@
+package com.example.websocket.HelloMessage;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+}
